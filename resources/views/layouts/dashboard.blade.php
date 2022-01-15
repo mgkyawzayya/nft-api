a5143988832b7ddc1ced040d5b3d7ba4be84d824
KyawZayya <kyawzayya.ucsp@gmail.com>
2022-01-15 18:22:48 +0630

    fix icon

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -85,13 +85,13 @@
                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
+                                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z">
                                         </path>
                                     </svg>
-                                   NFTs
+                                    NFTs
                                 </a>
                                 <a href="{{ route('categories.index') }}"
-                                    class="{{ Request::is('categories') || Request::is('categories/*')? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white ' }} group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                                    class="{{ Request::is('categories') || Request::is('categories/*') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white ' }} group flex items-center px-2 py-2 text-base font-medium rounded-md"
                                     x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
                                     <svg class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6"
                                         x-state-description="undefined: &quot;text-gray-300&quot;, undefined: &quot;text-gray-400 group-hover:text-gray-300&quot;"
