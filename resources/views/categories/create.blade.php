d849609d8a201ce40c8aef0813fe63f20eea7d80
KyawZayya <kyawzayya.ucsp@gmail.com>
2022-01-15 17:05:37 +0630

    remove unused code

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -35,10 +35,6 @@
                                             class="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                             Submit
                                         </button>
-                                        <button type="cancel"
-                                            class="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                                            Cancel
-                                        </button>
                                     </div>
                                 </form>
                             </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,25 +36,6 @@
                         </p>
                     </dd>
                 </div>
-                {{-- <div class="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
-                    <dt>
-                        <div class="absolute bg-indigo-500 rounded-md p-3">
-                            <svg class="h-6 w-6 text-white" x-description="Heroicon name: outline/users"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z">
-                                </path>
-                            </svg>
-                        </div>
-                        <p class="ml-16 text-sm font-medium text-gray-500 truncate">Total Price</p>
-                    </dt>
-                    <dd class="ml-16 pb-3 flex items-baseline sm:pb-4">
-                        <p class="text-2xl font-semibold text-gray-900">
-                            {{ number_format($total_price) }}
-                        </p>
-                    </dd>
-                </div> --}}
             </dl>
         </div>
     </div>
